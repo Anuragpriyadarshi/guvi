d91c857ee65618ceee1429e5324e60d5c0515a2c
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-03-22 14:38:41 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+	int main()
+{
+     int n, num, digit, rev = 0;
+     cin >> num;
+     n = num;
+     while(num!=0)
+     {
+         digit = num % 10;
+         rev = (rev * 10) + digit;
+         num = num / 10;
+     } 
+
+     if (n == rev)
+         cout << "yes";
+     else
+         cout << "no";
+
+    
+	return 0;
+}
