b74e4184c4552003f744229e52bc37cffa68c3e8
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-03-23 22:35:03 +0530

    Create multiple.cpp

---
+++ multiple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	cin>>n;
+	cout<<n*1<<" "<<n*2<<" "<<n*3<<" "<<n*4<<" "<<n*5;
+	return 0;
+}
