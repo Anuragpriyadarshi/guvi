5eb2873e0b84cba34c0ff22b45dead40700bb171
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-04-10 14:43:59 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -2,21 +2,16 @@
  
 int main()
 {
-  int n, first = 0, second = 1, next, c;
-  scanf("%d", &n);
- 
-  for (c = 0; c < n; c++)
-  {
-    if (c <= 1)
-      next = c;
-    else
-    {
-      next = first + second;
-      first = second;
-      second = next;
-    }
-    printf("%d ", next);
-  }
- 
-  return 0;
+int a=1,b=1,i,n,c;
+scanf("%d",&n);
+printf("%d ",a);
+printf("%d ",b);
+for(i=1;i<=n-2;i++)
+{
+c=a+b;
+printf("%d ",c);
+a=b;
+b=c;
+}
+return 0;
 }
