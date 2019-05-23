3a0e88a490c878e47727a452a74623b389972d05
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-05-24 00:19:01 +0530

    Create q209.c

---
+++ q209.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,count=0,i,flag;
+	scanf("%d %d",&a,&b);
+	 while (a <= b)
+    {
+        flag = 0;
+
+        for(i = 2; i <= a/2; ++i)
+        {
+            if(a% i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            count =count+1;
+
+        ++a;
+    }
+    printf("%d",count);
+	return 0;
+}
