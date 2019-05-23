cddf1689748a375eb0e758b682a0a735d0caf5b6
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-05-23 23:39:03 +0530

    Update q205.c

--- q205.c
+++ q205.c
@@ -5,11 +5,45 @@ int main(void) {
 	scanf("%c",&r);
 	 if (r == 'I') 
         printf("1");
+	 if (r == 'II')
+	  printf("2");
+	 if (r == 'III')
+	  printf("3");
+	 if (r == 'IV')
+	  printf("4");
     if (r == 'V') 
         printf("5");
+     if (r == 'VI')
+	printf("6");
+      if (r == 'VII')
+	printf("7");
+       if (r == 'VIII')
+	printf("8");
+	 if (r == 'IX')
+          printf("9");
     if (r == 'X') 
         printf("10");
-    if (r == 'L') 
+	 if (r == 'XI')
+          printf("11");
+	 if (r == 'XII')
+          printf("12");
+	 if (r == 'XIII')
+	 printf("13")
+	 if (r == 'XIV')
+	printf("14");
+	 if (r == 'XV')
+	printf("15");
+	 if (r == 'XVI')
+        printf("16");
+	if(r=='XVII')
+	   printf("17");
+	if(r=='XVIII')
+		printf("18");
+	 if (r == 'XIX')
+		 printf("19");
+	 if (r == 'XX')
+		 printf("20")   
+		if (r == 'L') 
         printf("50");
     if (r == 'C')
        printf("100");
