64a1bc82a3b9e179f0f916fb17b4027df820c255
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-07-21 10:07:16 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -4,10 +4,16 @@ s=[]
 for i in k:
 	if k.count(i)>1:
 		s.append(i)
-s=list(set(sorted(s)))		
-for i in range(len(s)-1):
-	print(s[i],end=' ')
-print(s[len(s)-1],end='')	
+s=list(set(sorted(s)))
+if len(s)>0:
+	for i in range(len(s)-1):
+		print(s[i],end=' ')
+	print(s[len(s)-1],end='')	
+else:
+	print("unique")
+	
+	
+		
 	
 	
 	
