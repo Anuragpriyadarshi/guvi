5c8fa27cfe39f46c1420c9494aced02519f6a904
Anuragpriyadarshi <48614479+Anuragpriyadarshi@users.noreply.github.com>
2019-07-21 10:04:33 +0530

    Create hunter1.py

---
+++ hunter1.py
@@ -0,0 +1,14 @@
+n=int(input())
+k=list(map(int,input().split()))
+s=[]
+for i in k:
+	if k.count(i)>1:
+		s.append(i)
+s=list(set(sorted(s)))		
+for i in range(len(s)-1):
+	print(s[i],end=' ')
+print(s[len(s)-1],end='')	
+	
+	
+	
+		
